d5aa10f72af96366a76612c4c75168925402fffe
JDOdea <55107471+JDOdea@users.noreply.github.com>
2023-03-04 14:46:22 -0600

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,17 @@
+print("\n-----Welcome to Mad Libs!-----\n")
+
+mainCharacter = input("Type in a name.\n")
+storeType = input("\nType in a type of store.\n")
+lovedItem = input("\nType in a noun that the main character loves.\n")
+scaryAnimal = input("\nType in a scary animal.\n")
+carriedItem = input("\nType in an item you have on you.\n")
+friendsName = input("\nType in the name of a friend.\n")
+uniformItem = input("\nType in a piece clothing.\n")
+
+print("\n" + mainCharacter + ", the " + lovedItem + "-lover, went to the " + storeType + " store.")
+print("On the way there, " + mainCharacter + " came across a hungry " + scaryAnimal + "!")
+print(mainCharacter + " reached for their trusty " + carriedItem + ", but had left it at " + friendsName + "'s house.")
+print("Without hope, " + mainCharacter + " waited for the end.")
+print("As the " + scaryAnimal + " was upon them, it simply kept walking forward, ignoring " + mainCharacter + " completely.")
+print("Walking away and struggling to put on a uniformed " + uniformItem + ", " + mainCharacter + " could only hear the " + scaryAnimal + " mutter:\n")
+print("\"I'm late for my shift at the " + storeType + " store!\"\n")
